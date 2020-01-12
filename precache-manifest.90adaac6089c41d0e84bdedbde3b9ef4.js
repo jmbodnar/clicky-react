d86255492e2401076b88b3fb651c344dc1de2746
jmbodnar <jmbodnar@protonmail.com>
2020-01-12 13:47:58 -0500

    Updates

---
+++ precache-manifest.90adaac6089c41d0e84bdedbde3b9ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42adc70b21d7617bc00200f1ca49011a",
+    "url": "/clicky-react/index.html"
+  },
+  {
+    "revision": "df7f930523fd251d98ea",
+    "url": "/clicky-react/static/js/2.789dc847.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-react/static/js/2.789dc847.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cee1069d4d2895b6fbde",
+    "url": "/clicky-react/static/js/main.72660a77.chunk.js"
+  },
+  {
+    "revision": "0e1655a83e9b6573a1a2",
+    "url": "/clicky-react/static/js/runtime-main.1ce1bc98.js"
+  }
+]);
