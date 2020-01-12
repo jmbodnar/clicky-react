ab61ab7849aaadf898a4b30bfa2c8e418b50ca9e
jmbodnar <jmbodnar@protonmail.com>
2020-01-12 14:27:56 -0500

    Updates

--- precache-manifest.c15bc9201486032fc828373279a7af78.js
+++ precache-manifest.c15bc9201486032fc828373279a7af78.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e648e7947998ffdbfb84211961243fc1",
+    "revision": "1d47741162e4669942f030da29532a98",
     "url": "/clicky-react/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clicky-react/static/js/2.789dc847.chunk.js.LICENSE"
   },
   {
-    "revision": "cee1069d4d2895b6fbde",
-    "url": "/clicky-react/static/js/main.72660a77.chunk.js"
+    "revision": "1d855bf38f0a5cfabb7e",
+    "url": "/clicky-react/static/js/main.4666e2bb.chunk.js"
   },
   {
     "revision": "0e1655a83e9b6573a1a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-react/precache-manifest.0b0226b2b726ddd0c95ed3d36a4d086a.js"
+  "/clicky-react/precache-manifest.c15bc9201486032fc828373279a7af78.js"
 );
 
 self.addEventListener('message', (event) => {
